e00c96d732d2debdc8e1cbe0517ab062223bb1ec
Pedro Matos <pedro_matos2012@hotmail.com>
2021-12-18 17:35:43 -0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+nome = input('Qual é o seu nome? ')
+print('Olá', nome, 'prazer em te conhecer!')
+
+dia = input('Qual o dia do seu nascimento? ')
+mês = input('Qual é o mês do seu nascimento? ')
+ano = input('Qual é o ano do seu nascimento? ')
+print('Você nasceu no dia', dia, 'de', mês, 'de', ano, '. Correto? ')
+
+numero1 = input('Digite um número! ')
+numero2 = input('Digite outro número! ')
+print('A soma é', numero1 + numero2)
