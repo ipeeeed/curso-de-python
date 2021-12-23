cab86e3135f651d6186a073cf4eac2f0bc18791b
Pedro Matos <pedro_matos2012@hotmail.com>
2021-12-22 20:29:15 -0400

    pytualijanela

---
+++ janela.py
@@ -0,0 +1,18 @@
+def testado():
+    mund = 'Olá mundo, teste para janelas'
+    texto_vazio['text'] = mund
+
+
+
+from tkinter import *
+
+janela = Tk()
+janela.geometry('250x400')
+janela.title('Programas')
+texto = Label(janela, text='Clique no botão')
+texto.grid(column=0, row=0, padx=80, pady=10)
+botao = Button(janela, text='enviar', command=(testado))
+botao.grid(column=0, row=1, padx=80, pady=10)
+texto_vazio = Label(janela, text='')
+texto_vazio.grid(column=0, row=2, padx=10, pady=10)
+janela.mainloop()
--- pythonexercicios/ex008.py
+++ pythonexercicios/ex008.py
@@ -28,5 +28,14 @@ if res != 0:
     print('ÍMPAR')
 else:
     print('PAR')
+
+
+d = float(input('Qual a distância da sua viagem? '))
+if d <= 200:   
+    p = d * 0.50
+else:
+    p = d * 0.45
+print('Você está prestes a começar uma viagem de {}Km'.format(d))
+print('E o preço da sua passagem será de R${:.2f}'.format(p))
 '''
 
