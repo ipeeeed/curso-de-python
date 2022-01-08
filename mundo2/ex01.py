48b54512e1434ee9569c89a8ce1697958c0a89e5
Pedro Matos <pedro_matos2012@hotmail.com>
2022-01-07 21:00:43 -0400

    Update ex01.py

--- mundo2/ex01.py
+++ mundo2/ex01.py
@@ -92,7 +92,7 @@ elif pri == seg == ter:
     print('Os segmentos acima PODEM FORMAR um triângulo EQUILÁTERO')
 elif pri == seg and pri != ter or seg == ter and seg != pri or ter == pri and ter != seg:
     print('Os segmentos acima PODEM FORMAR um triângulo ISÓSCELES')
-'''
+
 
 peso = float(input('Qual é o seu peso? (Kg) '))
 altura = float(input('Qual é a sua altura? (m) '))
@@ -108,3 +108,47 @@ elif imc >= 30 and imc < 40:
     print('Você está em OBESIDADE!')
 else:
     print('Você está em OBESIDADE MÓRBIDA, cuidado!')
+
+
+print('{:=^40}'.format(' Lojas IPED '))
+preço = float(input('Preço das compras: R$'))
+print("""
+FORMAS DE PAGAMENTO
+[ 1 ] à vista dinheiro/cheque
+[ 2 ] à vista cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão
+""")
+opção = int(input('Qual a opção? '))
+if opção == 1:
+    total = preço - (preço * (10 / 100))
+elif opção == 2:
+    total = preço - (preço * (5 / 100))
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R${:.2f}'.format(parcela))
+elif opção == 4:
+    total = preço + (preço * (20 / 100))
+    totalparc = int(input('Quantas parcelas? '))
+    parcela = total / totalparc
+    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS'.format(totalparc, parcela))
+else:
+    total = preço
+    print('[ERRO] Opção invalida de pagamento. Tente novamente!')
+print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, total))
+'''
+
+print("""
+Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA
+""")
+escolha = int(input('Qual é a sua jogada? '))
+import random
+jokenpo = random.randint(0, 2)
+if escolha == jokenpo:
+    print('EMPATE')
+elif escolha == 0 and jokenpo == 1:
+    
