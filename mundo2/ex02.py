6e38835c90e8162324b51f6804d4c4018f8b013a
Pedro Matos <pedro_matos2012@hotmail.com>
2022-01-10 00:58:23 -0400

    Update ex02.py

--- mundo2/ex02.py
+++ mundo2/ex02.py
@@ -1,6 +1,26 @@
-
+'''
 from time import sleep
 for c in range(10, -1, -1):
     print(c)
     sleep(1)
 print('BUM! BUM! POOOW!')
+
+
+for c in range(2, 51, 2):
+    if c % 2 == 0:
+        print(c, end=' ')
+print('Acabou!')
+
+
+soma = 0
+contador = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        contador = contador + 1
+        soma = soma + c
+#        print(c, end=' ')
+print('A soma de todos os {} valores solicitados é de {}.'.format(contador, soma))
+'''
+
+
+for c in range():
