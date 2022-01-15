d274f45f31633c20f230d457ae210a4ed3e83cbc
Pedro Matos <pedro_matos2012@hotmail.com>
2022-01-14 20:40:47 -0400

    Update ex02.py

--- mundo2/ex02.py
+++ mundo2/ex02.py
@@ -20,7 +20,28 @@ for c in range(1, 501, 2):
         soma = soma + c
 #        print(c, end=' ')
 print('A soma de todos os {} valores solicitados é de {}.'.format(contador, soma))
+
+
+n = int(input('Digite um número para ver sua tabuada: '))
+for c in range(1, 11):
+    print('{} x {} = {}'.format(n, c, n*c))
+
+
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('Digite o {} valor: '.format(c)))
+    if num % 2 == 0:
+        soma = soma + num
+        cont = cont + 1
+print('Você informou {} números PARES e a soma foi {}.'.format(cont, soma))
+
+
+ter = int(input('Primeiro termo: '))
+raz = int(input('Razão: '))
+dec = ter + (10 - 1) * raz
+for c in range(ter, dec + raz, raz):
+    print('{}'.format(c), end=' → ' )
+print('ACABOU')
 '''
 
-
-for c in range():
