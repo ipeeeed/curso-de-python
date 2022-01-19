dd093b68acb6f15e28e26c3cc8fd8998aa09a403
Pedro Matos <pedro_matos2012@hotmail.com>
2022-01-18 21:05:48 -0400

    Update ex03.py

--- mundo2/ex03.py
+++ mundo2/ex03.py
@@ -23,24 +23,37 @@ while n != pc:
 print('Acertou com {} tentativas. Parabéns, o PC pensou no número {}!'.format(erro, pc))
 '''
 
+from time import sleep
 opção = 0
+pri = int(input('Primeiro valor: \033[31m'))
+seg = int(input('\033[mSegundo valor: \033[31m'))
 while opção != 5:
-    pri = int(input('Primeiro valor: '))
-    seg = int(input('Segundo valor: '))
-    if opção == 1:
-        print('A soma entre {} + {} é {}'.format(pri, seg, (pri + seg)))
-    elif opção == 2:
-        print('O resultado entre {} x {} é {}'.format(pri, seg, (pri * seg)))
-    elif opção == 3:
-        print('Entre {} e {} o meior valor é {}'.format(pri, seg, ))
-    print("""
-    [ 1 ] somar
+    print("""\033[m    [ 1 ] somar
     [ 2 ] multiplicar
     [ 3 ] maior
     [ 4 ] novos números
     [ 5 ] sair do programa""")
-    opção = int(input('Qual é a sua opção? '))
-from time import sleep
+    opção = int(input('>>>>> Qual é a sua opção? \033[31m'))
+    sleep(2)
+    if opção == 1:
+        print('\033[mA soma entre \033[33m{}\033[m e \033[33m{}\033[m é \033[33m{}'.format(pri, seg, (pri + seg)))
+    elif opção == 2:
+        print('\033[mO resultado entre \033[33m{}\033[m x \033[33m{}\033[m é \033[33m{}'.format(pri, seg, (pri * seg)))
+    elif opção == 3:
+        if pri > seg:
+            maior = pri
+        elif pri < seg:
+            maior = seg
+        else:
+            maior = 'nenhum'
+        print('\033[mEntre \033[33m{}\033[m e \033[33m{}\033[m o maior valor é \033[33m{}\033[m'.format(pri, seg, maior))
+    elif opção == 4:
+        print('\033[mInforme os números novamente!')
+        pri = int(input('Primeiro valor: \033[31m'))
+        seg = int(input('\033[mSegundo valor: \033[31m'))
+    else:
+        print('\033[mOpção inválida. Tente novamente!')
+    print('\033[m',15*'=-=')
 print('Finalizando...')
 sleep(2.4)
 print('Fim do programa! Volte sempre!')
