d45f8a7fa01371826eeebe50b0cb75945470d56e
Pedro Matos <pedro_matos2012@hotmail.com>
2022-01-22 00:16:57 -0400

    Update ex01.py

--- mundo3/ex01.py
+++ mundo3/ex01.py
@@ -1,4 +1,9 @@
-num = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+''''''
 ext = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-n = 0
 n = int(input('Digite um número entre 0 e 20: '))
+for contador, numero in enumerate(ext):
+    if n == contador:
+        n = numero
+        break
+#    print(f'O número {contador} é contado como {numero}')
+print(f'O numero é o {n}')
