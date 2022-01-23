ce1bde1ddabeeead0c20f29761173049b1088988
Pedro Matos <pedro_matos2012@hotmail.com>
2022-01-23 12:32:29 -0400

    Update ex01.py

--- mundo3/ex01.py
+++ mundo3/ex01.py
@@ -1,9 +1,8 @@
 ''''''
-ext = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-n = int(input('Digite um número entre 0 e 20: '))
-for contador, numero in enumerate(ext):
-    if n == contador:
-        n = numero
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= n <= 20:
         break
-#    print(f'O número {contador} é contado como {numero}')
-print(f'O numero é o {n}')
+    print('Tente novamente. ', end='')
+print(f'Você digitou o número {extenso[n]}')
