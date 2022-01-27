8bde681549cab64ecb063536f0c115ebe8c14de8
Pedro Matos <pedro_matos2012@hotmail.com>
2022-01-27 18:35:52 -0400

    Update ex01.py

--- mundo3/ex01.py
+++ mundo3/ex01.py
@@ -1,4 +1,4 @@
-''''''
+'''
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while True:
     n = int(input('Digite um número entre 0 e 20: '))
@@ -6,3 +6,59 @@ while True:
         break
     print('Tente novamente. ', end='')
 print(f'Você digitou o número {extenso[n]}')
+
+
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense', 'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia', 'São Paulo', 'Fluminense', 'Sport Recife', 'EC Vitória', 'Curitiba', 'Avaí', 'Ponte Preta', 'Atlético-GO')
+print('-='*15)
+print(f'Lista de times do Brasileirão: {times}')
+print('-='*15)
+print(f'Os 5 primeiros são: {times[0:5]}')
+print('-='*15)
+print(f'Os 4 últimos são {times[-4:]}')
+print('-='*15)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-='*15)
+print(f'O Chapecoense está na {times.index("Chapecoense")+1}ª posição')
+print('-='*15)
+
+
+from random import randint
+num = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print(f'Os valores sorteados foram: ', end='')
+for n in num:
+    print(f'{n} ', end='')
+print(f'\nO maior valor sorteado foi {max(num)}')
+print(f'O menor valor sorteado foi {min(num)}')
+
+
+num = (int(input('Digite um número: ')), int(input('Digite outro número: ')), int(input('Digite mais um número: ')), int(input('Digite o último número: ')))
+print(f'Você digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1}ª posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+print(f'Os valores pares digitados foram', end=' ')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' ')
+
+
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
+listagem = ('Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25, 'Transferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-'*40)
+'''
+
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+for p in palavras:
+    print(f'\nNa palavra {p} temos ', end='')
+    for letra in p:
+        if letra.upper() in 'AEIOU':
+            print(letra.lower(), end=' ')
